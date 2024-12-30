3d1a66aa5d608430db83c439c4317deba5e2b4ea
ilyas-hub <91405460+ilyas-hub@users.noreply.github.com>
2024-12-30 15:39:11 +0530

    change in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-// vite.config.js
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 10000,  // or any port available on Render
-    host: '0.0.0.0',  // Listen on all network interfaces
+    port: 10000,      // or any port Render expects
+    host: '0.0.0.0',  // Ensures app is accessible from the network
   },
-};
+});
